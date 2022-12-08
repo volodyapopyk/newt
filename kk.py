1713bdc4a210ecac157bcbc78ae13d421bcc1811
volodyapopyk <vova.popyk@mail.ru>
2022-12-08 22:46:00 +0300

    finally

---
+++ kk.py
@@ -0,0 +1,39 @@
+#===========================
+c = "I Love Python\n" *42
+print(c)
+#===========================
+age_in_mounth = 12*24
+print(age_in_mounth, end="\n\n")
+#===========================
+age_in_years = int(288/12)
+print(int(age_in_years))
+#===========================
+name = "Volodymyr"
+my_age = "\nMy name is " + name + ", I'm "+str(age_in_years) + " year old\n"
+print(my_age)
+#===========================
+a=1
+b=2
+c=a==b
+print(c)
+c=a>b
+print(c)
+c=a<b
+print(c)
+c=b>=a
+print(c)
+c=b<=a
+
+print(c, end="\n\n")
+#===========================
+a = 2
+
+b = 5
+
+c = 6
+
+d = str(a)+str(b)+str(c)
+print(d)
+results=type(d)
+print(results)
+#===========================
