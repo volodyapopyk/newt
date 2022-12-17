f09ff6aa900fec9ec36fad9f3e369f1cef1762e2
volodyapopyk <vova.popyk@mail.ru>
2022-12-17 21:59:27 +0300

    new

---
+++ new.py
@@ -0,0 +1,33 @@
+import time
+while True:
+    a = input("\nWrite something:")
+    if a.isdigit():
+        if int(a) % 2 == 0:
+                print(f'\nThis is a number')
+                print(f'Is even')
+        else:
+                print(f'\nThis is a number')
+                print(f'Is odd')
+
+    elif a.isalpha():
+        if a.isupper():
+                print(f'\nThis is a text')
+                print(f'It is a сapital letter')
+
+        elif a.islower():
+                print(f'\nThis is a text')
+                print(f'It is a small letter')
+
+    elif a.isascii():
+                print('\nThis is a symbol')
+
+    c = input("\nDo you want to continue? [Y/N] or another program [a]:")
+    if c == ("Y"):
+        continue
+    if c == "a":
+        while True:
+                print("I love Python")
+                time.sleep(4.2)
+    else:
+        print("\n Bye ")
+        break
